41c698b88d49d6fc1fa967323e14a343c710a269
femmytedrey <femmytedrey@gmail.com>
2023-12-13 17:13:54 +0100

    work in progress

--- src/App.js
+++ src/App.js
@@ -7,12 +7,31 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [ loading, setLoading ] = useState(true)
   const [ tours, setTours ] = useState([])
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+    
+  }
+  useEffect(() => {
+    fetchTours();
+  },[])
   if(loading){
     return <main>
-      {loading}
+      <Loading />
     </main>
   }
-  return <h2>Tours Project Setup</h2> 
+  
+  return <main>
+    <Tours tours={tours} />
+  </main> 
 }
 
 export default App
