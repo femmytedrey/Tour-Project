9cc7537f6ac4d29118446ad83abf93609e078a36
femmytedrey <femmytedrey@gmail.com>
2023-12-13 17:45:52 +0100

    refresh button added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  const [ loading, setLoading ] = useState(true)
-  const [ tours, setTours ] = useState([])
+  const [ loading, setLoading ] = useState(true);
+  const [ tours, setTours ] = useState([]);
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  }
   const fetchTours = async () => {
     setLoading(true);
     try {
@@ -28,9 +32,17 @@ function App() {
       <Loading />
     </main>
   }
+  if(tours.length === 0){
+    return <main>
+      <div className='title'>
+        <h2>no tours left</h2>
+        <button className='btn' onClick={fetchTours}>refresh</button>
+      </div>
+    </main>
+  }
   
   return <main>
-    <Tours tours={tours} />
+    <Tours tours={tours} removeTour={removeTour} />
   </main> 
 }
 
--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
-const Tour = ({ id, image, info, price, name }) => {
-
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [readMore, setReadMore] = useState(false)
   return <article className='single-tour'>
     <img src={image} alt={name}/>
     <footer>
@@ -10,8 +10,12 @@ const Tour = ({ id, image, info, price, name }) => {
         <h4 className='tour-price'>${price}</h4>
       </div>
       <p>
-        {info}
+        {readMore ? info : `${info.substring(0, 200)}...`}
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? 'show less' : 'show more'}
+        </button>
       </p>
+      <button className='delete-btn' onClick={()=> removeTour(id)}>not interested</button>
     </footer>
   </article>;
 };
